4a1d3d55131517e7343d062153561ff0b7fd4962
selmanfariz18 <salmanulfarizyka@gmail.com>
2022-11-26 20:32:02 +0530

    hello project up

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div id="app">hello {{name}}</div>
+</template>
+
+<script>
+
+
+export default {
+  name: "App",
+  data(){
+    return{
+      name:"selman"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.all{
+  text-align: center;
+}
+</style>
